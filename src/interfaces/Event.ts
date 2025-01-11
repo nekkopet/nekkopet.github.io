54c1a996af99027fa8784c332eafd0b854d77580
Daveed <daveed@santocabron.com>
2025-01-11 01:18:53 -0500

    Created basic template to display Contet type event

---
+++ src/interfaces/Event.ts
@@ -0,0 +1,10 @@
+export default interface Event {
+  title: string;
+  Description: string;
+  startDate: string;
+  endDate: string;
+  PRICES: any;
+  Thumbnail: any;
+  SEO: any;
+  Slides: any;
+}
