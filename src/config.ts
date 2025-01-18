dd7b0202f03d61463a6431c9756c0f3fa1bca717
Daveed <daveed@santocabron.com>
2025-01-17 23:29:37 -0500

    clean up 404 page for a more clear format

--- src/config.ts
+++ src/config.ts
@@ -14,6 +14,7 @@ export type markketConfig = {
     accent: string;
   };
   POSTHOG_ID: string;
+  store?: Store;
 };
 
 
@@ -50,6 +51,7 @@ export const markketplace: markketConfig = {
     accent: import.meta.env.COLOR_ACCENT as string || '#38b2ac',
   },
   POSTHOG_ID: import.meta.env.POSTHOG_ID as string || '',
+  store: StoreData,
 };
 
 console.log('Markket', { markketplace });
