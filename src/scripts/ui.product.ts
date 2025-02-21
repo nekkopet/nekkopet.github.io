011815bff1de4c690fb58a6692a602a67ba9eb6c
Daveed <daveed@santocabron.com>
2025-02-21 16:14:13 -0500

    changed color link in blog post

--- src/scripts/ui.product.ts
+++ src/scripts/ui.product.ts
@@ -1,23 +1,19 @@
-// import { type Product } from '../interfaces';
 import { markketplace } from '@config';
+
 /**
- *
+ * Allows to click on a thumbnail and change the main product image
  */
 export const ProductSlideshow = function () {
-  // Front matter TS is executed in the ~server during the build process
-  // import confetti from 'canvas-confetti';
 
   const buttons = document.querySelectorAll("[data-astro-image]");
 
-  // Add event listeners to fire confetti when a button is clicked.
   buttons.forEach(button => {
     button.addEventListener("click", e => {
       const jsonData = (e.target as Element)?.getAttribute('data-astro-image');
 
       try {
-        const jsonObject = JSON.parse(jsonData || ''); // Parse the JSON string
+        const jsonObject = JSON.parse(jsonData || '');
         const heroImage = document.querySelector(".hero-image");
-        console.log({ heroImage })
 
         if (heroImage) {
           (heroImage as HTMLImageElement).src = jsonObject.url;
@@ -52,6 +48,7 @@ export type PaymentLinkOptions = {
 /**
  * Sends a POST request to the API, to request a unique stripe payment link
  * We redirect buyers there to complete the purchase
+ * @TODO move to markket-next and import via npm
  *
  * @returns {Promise<Response>} Payment link
  */
