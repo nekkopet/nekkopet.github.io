c1cc5e789f63564ee15437a94cafec9b170d3fed
Daveed <daveed@santocabron.com>
2025-07-16 23:31:07 -0400

    article page components

--- src/interfaces/Article.ts
+++ src/interfaces/Article.ts
@@ -1,29 +1,3 @@
-export interface Tag {
-  Label: string;
-  Color: string;
-}
+import type { CollectionEntry } from "astro:content";
 
-export interface SEOImage {
-  url: string;
-  alternativeText: string | null;
-  width: number;
-  height: number;
-}
-
-export interface SEO {
-  metaTitle: string;
-  metaDescription: string;
-  socialImage?: SEOImage;
-}
-
-export default interface Article {
-  slug: string;
-  updatedAt: string;
-  Title: string;
-  cover: any;
-  SEO: any;
-  tags?: Tag[];
-  Tags?: Tag[];
-  createdAt: string;
-
-}
+export type Article = CollectionEntry<"posts">;
